563ce78a51497193992ee48df5bf6a02d160de64
Larkwor <rosesarahglover@gmail.com>
2023-05-11 15:27:38 -0700

    modules

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-from add_0 import add
+from magic_calculation_102 import add, sub
 
-a = 1
-b = 2
-
-print("{} + {} = {}".format(a, b, add(a, b)))
+def magic_calculation(a, b):
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return c
+    else:
+        return sub(a, b)
