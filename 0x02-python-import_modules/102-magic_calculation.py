5f9a67729aa38f78ba92e8e9b1fabc1b23dde85f
Larkwor <rosesarahglover@gmail.com>
2023-05-11 15:51:39 -0700

    import

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,9 +1,19 @@
+#!/usr/bin/python3
+# 102-magic_calculation.py
+
+# Import the add and sub functions from the magic_calculation_102 module
+from magic_calculation_102 import add, sub
+
 def magic_calculation(a, b):
-    add, sub =  __import__('magic_calculation_102', fromlist=['add', 'sub'])
+    """Performs a magic calculation based on the given arguments."""
+
+    # If a is less than b, perform a series of additions and return the result
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
         return c
+
+    # If a is not less than b, perform a subtraction and return the result
     else:
         return sub(a, b)
