49cb8d7368dcbeeb5397a455c2608f04a08c3966
Larkwor <rosesarahglover@gmail.com>
2023-05-17 15:18:49 -0700

    1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -7,10 +7,4 @@ def search_replace(my_list, search, replace):
             new_list.append(replace)
         else:
             new_list.append(item)
-    return new_list
-
-my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
-new_list = search_replace(my_list, 2, 89)
-
-print(new_list)
-print(my_list)`
+    return new_list`
