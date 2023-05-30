fb9a539740374dbe43fa1bf14b4810bc0fbb9076
Larkwor <rosesarahglover@gmail.com>
2023-05-30 16:08:34 -0700

    division

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def raise_exception():
+    # Perform an operation that raises a TypeError
+    # For example, adding a string to an integer
+    value = "Hello" + 42
+
+    # Raise the TypeError explicitly
+    raise TypeError("Exception raised")
