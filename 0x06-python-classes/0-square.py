c5874779ce9fe44c6ba53124549eb4d061feff64
Larkwor <rosesarahglover@gmail.com>
2023-05-31 16:11:06 -0700

    commit

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
 class Square:
+    """Represents a square"""
     def __init__(self):
-        pass
+        self.dict_ = {}
+
+mysquare = Square()
+print(type(mysquare))
+print(mysquare.dict_)
